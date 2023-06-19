30a76470c14512d7008dd3fee7ff8763173b67c8
Lummiense <Chumichael@mail.ru>
2023-06-19 17:40:00 +0300

    add sjme tests

---
+++ src/Otus.Teaching.PromoCodeFactory.UnitTests/WebHost/Builders/PartnerBuilder.cs
@@ -0,0 +1,50 @@
+﻿using Otus.Teaching.PromoCodeFactory.Core.Domain.PromoCodeManagement;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Otus.Teaching.PromoCodeFactory.UnitTests.WebHost.Builders
+{
+    public static class PartnerBuilder
+    {
+        public static Partner Build()
+        {
+            return new Partner()
+            {
+                Id = Guid.Parse("7d994823-8226-4273-b063-1a95f3cc1df8"),
+                Name = "Суперигрушки",
+                IsActive = true,
+                PartnerLimits = new List<PartnerPromoCodeLimit>()
+                {
+                    new PartnerPromoCodeLimit()
+                    {
+                        Id = Guid.Parse("e00633a5-978a-420e-a7d6-3e1dab116393"),
+                        CreateDate = new DateTime(2020,07,9),
+                        EndDate = new DateTime(2020,10,9),
+                        Limit = 100
+                    }
+                }
+            };
+        }
+
+        public static Partner WithLimit(this Partner partner)
+        {
+           
+            partner.PartnerLimits = new List<PartnerPromoCodeLimit>();
+            partner.PartnerLimits.Add(new PartnerPromoCodeLimit()
+            {
+                Id = Guid.NewGuid(),
+                CreateDate = new DateTime(2023, 01, 01),
+                EndDate = new DateTime(2023,09,01),
+                Limit = 5
+            }) ;
+            return partner;
+        }
+        public static Partner ResetPromoCount(this Partner partner)
+        {
+            partner.NumberIssuedPromoCodes = 0;
+            return partner;
+        }
+    }
+}
--- src/Otus.Teaching.PromoCodeFactory.UnitTests/WebHost/Controllers/Partners/SetPartnerPromoCodeLimitAsyncTests.cs
+++ src/Otus.Teaching.PromoCodeFactory.UnitTests/WebHost/Controllers/Partners/SetPartnerPromoCodeLimitAsyncTests.cs
@@ -5,6 +5,11 @@ using Otus.Teaching.PromoCodeFactory.Core.Abstractions.Repositories;
 using Otus.Teaching.PromoCodeFactory.Core.Domain.PromoCodeManagement;
 using Otus.Teaching.PromoCodeFactory.WebHost.Controllers;
 using Xunit;
+using Otus.Teaching.PromoCodeFactory.UnitTests.WebHost.Builders;
+using System;
+using Otus.Teaching.PromoCodeFactory.WebHost.Models;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Otus.Teaching.PromoCodeFactory.UnitTests.WebHost.Controllers.Partners
 {
@@ -19,10 +24,69 @@ namespace Otus.Teaching.PromoCodeFactory.UnitTests.WebHost.Controllers.Partners
             _partnersRepositoryMock = fixture.Freeze<Mock<IRepository<Partner>>>();
             _partnersController = fixture.Build<PartnersController>().OmitAutoProperties().Create();
         }
-        
+
+        private Partner CreateBasePartner()
+        {
+            var partner = PartnerBuilder.Build();
+            return partner;
+        }
+
+
         //Партнер не найден.
         [Fact]
         public async void SetPartnerPromoCodeLimit_PartnerNotFound_ReturnsNotFound()
+        {
+            //arrange
+            Guid partnerId = Guid.NewGuid();
+            var request = new Fixture().Create<SetPartnerPromoCodeLimitRequest>();
+            Partner partner = null;
+            _partnersRepositoryMock.Setup(x => x.GetByIdAsync(partnerId)).ReturnsAsync(partner);
+
+            //Act
+            var result = await _partnersController.SetPartnerPromoCodeLimitAsync(partnerId, request);
+
+            //Assert
+            result.Should().BeAssignableTo<NotFoundResult>();
+        }
+
+        //Партнер заблокирован
+        [Fact]
+        public async void SetPartnerPromoCodeLimit_PartnerBlock_ReturnsBadRequest()
+        {
+            //arrange
+            Guid partnerId = Guid.Parse("7d994823-8226-4273-b063-1a95f3cc1df8");
+            var request = new Fixture().Create<SetPartnerPromoCodeLimitRequest>();
+            var partner = CreateBasePartner();
+            partner.IsActive = false;
+            _partnersRepositoryMock.Setup(x => x.GetByIdAsync(partnerId)).ReturnsAsync(partner);
+
+
+            //Act
+            var result = await _partnersController.SetPartnerPromoCodeLimitAsync(partnerId, request);
+
+            //Assert
+            result.Should().BeAssignableTo<BadRequestObjectResult>();
+        }
+
+        //Обнуление количества выданных промокодов при выставлении лимита
+        [Fact]
+        public async void SetPartnerPromoCodeLimit_AddLimit_ShouldResetPromoCounter()
+        {
+            //Arrange
+            var partner = CreateBasePartner().WithLimit();
+            var request = new Fixture().Create<SetPartnerPromoCodeLimitRequest>();
+            _partnersRepositoryMock.Setup(x => x.GetByIdAsync(partner.Id)).ReturnsAsync(partner);
+            var expectedPartner = CreateBasePartner().WithLimit();
+
+            //Act
+            await _partnersController.SetPartnerPromoCodeLimitAsync(partner.Id, request);
+
+            //Assert
+            partner.NumberIssuedPromoCodes.Should().Be(expectedPartner.NumberIssuedPromoCodes);
+        }
+
+        //Отключение предыдущего лимита при установке нового
+        public async void SetPartnerPromoCodeLimit_AddLimit_SholdResetPreviousLimit()
         {
 
         }
